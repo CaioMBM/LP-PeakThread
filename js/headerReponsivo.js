fd67dac3539b3740a959453c24b5f5a2a8e85ea4
Caio Montenegro <caiombmonteiro@gmail.com>
2025-12-05 00:24:49 -0300

    Aula 5_11 - Criação da responsividade da Headline

--- js/headerReponsivo.js
+++ js/headerReponsivo.js
@@ -4,18 +4,26 @@ const navLinks    = document.querySelector('.container-navLinks');
 const navLinksA   = document.querySelectorAll('.navLink');
 const widthMobile = window.matchMedia('(width: 440px)');
 
-// Abrir menu
-abrirMenu.addEventListener('click', ()=> {
-    abrirMenu.style.display  = 'none';
-    fecharMenu.style.display = 'block';
-    navLinks.style.display   = 'block';
-})
-
 // Media query: 440px
 if (widthMobile.matches){
+    // Abri menu ao clicar no icon de menu hamburguer
+    abrirMenu.addEventListener('click', ()=> {
+        abrirMenu.style.display  = 'none';
+        fecharMenu.style.display = 'block';
+        navLinks.style.display   = 'block';
+    })
+
+    // Fecha menu ao clicar no icon de X
+    fecharMenu.addEventListener('click', ()=> {
+        abrirMenu.style.display  = 'block';
+        fecharMenu.style.display = 'none';
+        navLinks.style.display   = 'none';
+    })
+
+    // Adiciona evento de clique em todos os nav-links
     navLinksA.forEach(function(navlink){
         
-        // Fechar menu
+        // Fecha menu (fecha após 3 décimos de segundos após clicar em algum nav-link)
         navlink.addEventListener('click', () => {
             setTimeout(() => {
                 abrirMenu.style.display  = 'block';
